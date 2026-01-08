ac4d8ec74f3cf224844fffbe9f99df2a13dcdb27
Camilla Colombo Serri <camilla.colomboserri@wallapop.com>
2026-01-08 22:41:05 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
   },
-  server: {
-    port: 3000
-  }
 });
