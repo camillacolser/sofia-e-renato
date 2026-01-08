1f9e1022c3d69eadd1d187a5c8d12de099c201a6
Camilla Colombo Serri <camilla.colomboserri@wallapop.com>
2026-01-08 22:49:45 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Use '/' for custom domains. If using username.github.io/repo, use '/repo/'
-  base: '/',
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
