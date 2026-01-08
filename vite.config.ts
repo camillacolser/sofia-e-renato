64a2c819e960342e0e6c0a7e81290427b1dcee13
Camilla Colombo Serri <camilla.colomboserri@wallapop.com>
2026-01-08 22:45:43 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // Use '/' for custom domains. If using username.github.io/repo, use '/repo/'
+  base: '/',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
+    emptyOutDir: true,
   },
 });
